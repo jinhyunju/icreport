92039d2cf34c21e74d3ad819c20d47c9c41ee838
Jin-Lab workstation <jinhyun.ju@gmail.com>
2015-01-28 14:44:50 -0500

    Fixed anova factor vs continuous variable issue

--- R/component_association_test.R
+++ R/component_association_test.R
@@ -19,11 +19,11 @@ component_association_test <- function(input.A, info.input, covar.names){
       pval.mx <- matrix(NA, nrow = n.components, ncol = n.covariates)
       colnames(pval.mx) <- covar.names
 
-      covar.mx <- as.matrix(info.input[colnames(input.A),covar.names])
+      covar.df <- data.frame(info.input[colnames(input.A),covar.names])
 
       for( i in 1: dim(input.A)[1]){
         for (j in 1:length(covar.names)){
-          analysis.df <- data.frame("IC"=input.A[i,],"Covar" = covar.mx[,j])
+          analysis.df <- data.frame("IC"=input.A[i,],"Covar" = covar.df[,j])
           anova.fit <- aov(IC ~ Covar, data=analysis.df)
           model.fstat <- summary.lm(anova.fit)$fstatistic
           p.val <- pf(model.fstat[1],model.fstat[2],model.fstat[3], lower.tail = F, log.p = F)
