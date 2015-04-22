eac324e3365351de93062596d851ad335601d6d5
Jin-Lab workstation <jinhyun.ju@gmail.com>
2015-04-22 12:04:24 -0400

    Combined utility functions into two files

---
+++ R/plotting_functions.R
@@ -0,0 +1,163 @@
+#' Plotting a histogram of gene weights of a single component
+#'
+#' ggplot function for plotting a histogram for a single component
+#'
+#' @param s A single component  \code{s}
+#' @param plot.title Character title of the plot\code{plot.title}
+#' @return ggplot object
+#'
+#' @keywords keywords
+#'
+#'
+#' @examples
+#' R code here showing how your function works
+#' @export
+plot_component_hist <- function(s,plot.title){
+  G <- length(s)
+  plot.sig <- data.frame(idx = c(1:G), sig = s)
+  p <- ggplot(plot.sig, aes(x = sig)) +geom_histogram(aes(y = ..density..)) + xlab("Gene Weights") +
+    labs(title = plot.title) + geom_density() + ylab("") + coord_flip()
+  return(p)
+
+}
+
+
+#' Plotting the gene weights of a single component
+#'
+#' ggplot function for plotting a single component
+#'
+#' @param s A single component  \code{s}
+#' @param plot.title Character title of the plot\code{plot.title}
+#' @return ggplot object
+#'
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#' R code here showing how your function works
+plot_component <- function(s, plot.title, peaks = FALSE, peakresult = NULL, gene.names = NULL){
+  if(peaks == FALSE){
+    G <- length(s)
+    #plot.sig <- data.frame(idx = seq(1,G,1), sig = s)
+    plot.sig <- data.frame(idx = rank(-s, ties.method = "first"), sig = s)
+    p <- ggplot(plot.sig, aes(x=idx, y= sig)) +
+      geom_linerange(aes(ymin=0, ymax=sig)) +
+      xlab("Gene index") +
+      ylab("Gene Weights")+ labs(title = plot.title)
+    return(p)
+  } else {
+    G <- length(s)
+    colnames(s) <- "sig"
+    significant.peaks <- names(peakresult)
+    n.peaks <- length(peakresult)
+    peak.idx <- 1 * (gene.names %in% significant.peaks)
+    #plot.sig <- data.frame(idx = c(1:G), sig = s, peaks = peak.idx)
+    plot.sig <- data.frame(idx = rank(-s, ties.method = "first"), sig = s, peaks = peak.idx)
+    plot.title <- paste(plot.title,"_",n.peaks,"peaks", sep = " ")
+    p <- ggplot(plot.sig, aes(x=idx, y= sig)) + geom_linerange(aes(ymin=0, ymax=sig, colour = factor(peaks))) +
+      scale_y_continuous(expand=c(0,0))+scale_color_manual(values=c("black", "red")) + xlab("Genes sorted by weights") + ylab("Gene Weights")+
+      labs(title = plot.title) +theme(legend.position="none")
+    return(p)
+  }
+
+}
+
+#' Plotting the coefficients for a single IC
+#'
+#' ggplot function for plotting coefficients for a single IC
+#'
+#' @param info_input A dataframe that has the index and values of ICs with covariate information saved.
+#' @param k.col The column name to use for plot coloring.
+#' @return ggplot object
+#'
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#' R code here showing how your function works
+plot_coeff_w_legend <- function(info_input,k.col){
+
+  if(k.col != "none"){
+    ggplot(info_input, aes(x=idx, y= IC)) +
+      geom_point(aes_string(colour = k.col),shape=20, size = 3) +
+      xlab("sample") + ylab("IC coefficient")
+  } else {
+    ggplot(info_input, aes(x=idx, y= IC)) +
+      geom_point(aes(color = "black"),shape=20, size = 3) +
+      xlab("sample") + ylab("IC coefficient")
+  }
+
+}
+
+#' Plotting multiple ggplots
+#'
+#' Equivalent function for ggplots to setting par(mfrow=c(x,y)) for
+#' basic R plots.
+#' Adapted from : http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#'
+#' @param plotlist list of plots
+#' @param cols number of columns
+#' @param layout layout matrix for custom layout
+#' @return single plot object
+#'
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#' R code here showing how your function works
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+  if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+#' Adding customized black and white theme to ggplot objects and controls the font size.
+#'
+#' The function takes a ggplot object as an input and modifies the theme of it
+#'
+#' @param inputplot A ggplot object.  \code{s}
+#' @return A ggplot object with a custom theme added
+ggplot_add_theme <- function(inputplot){
+  inputplot <- inputplot + theme(axis.text = element_text(size=12),
+                                 axis.title=element_text(size=14,face="bold"),
+                                 title = element_text(size=16,face="bold"),
+                                 legend.title = element_text(size= 10, face="bold"),
+                                 legend.text = element_text(size = 8),
+                                 legend.key = element_rect(fill = NA,color = "white"),
+                                 panel.grid.major = element_line(colour = "grey80", linetype="dashed"),
+                                 panel.background = element_rect(fill = NA,color = "grey80"))
+  return(inputplot)
+}
---
+++ R/post_IC_function_bundle.R
@@ -0,0 +1,70 @@
+#' Testing the association between independent component coefficients and known covariates.
+#'
+#' The function takes in the A matrix from the ica.result object
+#' with a dataframe of measured covariates to test the association
+#' between them.
+#'
+#' @param A The mixing matrix estimated through ICA   \code{A}
+#' @param info.df A dataframe that holds the measured covariates for each sample \code{info.df}
+#' @return output A matrix holding the p-values for each indepenedent component and covariate pair.
+#' @keywords keywords
+#'
+#' @export
+#'
+component_association_test <- function(input.A, info.input, covar.names){
+
+  n.components <- dim(input.A)[1]
+  n.covariates <- length(covar.names)
+
+  pval.mx <- matrix(NA, nrow = n.components, ncol = n.covariates)
+  colnames(pval.mx) <- covar.names
+
+  covar.df <- data.frame(info.input[colnames(input.A),covar.names])
+
+  for( i in 1: dim(input.A)[1]){
+    for (j in 1:length(covar.names)){
+      analysis.df <- data.frame("IC"=input.A[i,],"Covar" = covar.df[,j])
+      anova.fit <- aov(IC ~ Covar, data=analysis.df)
+      model.fstat <- summary.lm(anova.fit)$fstatistic
+      p.val <- pf(model.fstat[1],model.fstat[2],model.fstat[3], lower.tail = F, log.p = F)
+      pval.mx[i,j] <- p.val
+    }
+  }
+  return(pval.mx)
+}
+
+#' Labeling peaks for each independent component
+#'
+#' The function takes in the A matrix from the ica.result object
+#' with a dataframe of measured covariates to test the association
+#' between them.
+#'
+#' @param s A single column of the S matrix with dimensions 1 x g  \code{s}
+#' @return A vector that contains the gene weights of the signficant peaks for a single independent component sorted
+#' in the decreasing order of absolute magnitude.
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#' R code here showing how your function works
+peak_detection <- function(ica.input.s){
+  peaks.idx <- which(abs(ica.input.s) > (2 * sd(ica.input.s))) # get the peak indexes
+  peaks <- ica.input.s[names(sort(abs(ica.input.s[peaks.idx]), decreasing = T))]
+  # sort them in decreasing order of absolute magnitude
+  return(peaks)
+}
+
+#' Calculating the variation explained by each component
+#'
+#' The function takes in a single row of the ICA result A matrix and a
+#' single column of the ICA result S matrix and calculates the sums of squares.
+#'
+#' @param s A single column of the S matrix with dimensions 1 x g  \code{s}
+#' @param a A single row of the A matrix with dimensions N x 1 \code{a}
+#' @return A single number of how much variantion is explained by a single IC.
+#'
+IC_variance_calc <- function(s, a){
+  var.IC <- sum(  (as.matrix(s) %*% t(as.matrix(a) ) )^2)
+  return(var.IC)
+}
