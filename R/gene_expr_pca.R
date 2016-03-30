a10fbd3cacc11a93e92df45d867847a0282c16d4
Jin-Lab workstation <jinhyun.ju@gmail.com>
2016-03-30 15:15:45 -0400

    Adding functionality for genotype processing and generalizing messages.

--- R/gene_expr_pca.R
+++ R/gene_expr_pca.R
@@ -125,3 +125,129 @@ gene_expr_pca <- function(phenotype.mx = NULL, info.df = NULL, check.covars = NU
     attr(pca.result, 'method') <- "pca"
     return(pca.result)
 }
+
+#' Custom PCA function for analyzing gene expression data.
+#'
+#' Performing PCA on a dataset and create a list object with results.
+#'
+#' @param genotype.mx Genotype matrix with diemnsions g x N
+#' @param info.df Dataframe that holds sample covariates (ex. population, gender, age, etc...)
+#' @param check.covars Column names of info.df which hold the covariates
+#' that should be used for association testing with IC coefficients.
+#' @param cor.threshold Threshold for significant correlation calling. Default is set to 0.05.
+#' @return List with the following entries.
+#' @keywords keywords
+#'
+#' @import mclust
+#' @export
+#'
+genotypes_pca <- function(genotype.mx = NULL, info.df = NULL, check.covars = NULL,
+                          cor.threshold = 0.05){
+
+    if(is.null("genotype.mx")){
+      message("Please specify genotype matrix \n")
+      break;
+    }
+
+    geno.nrow <- nrow(genotype.mx)
+    geno.ncol <- ncol(genotype.mx)
+    message("genotype.mx dimensions = ", geno.nrow , " x ", geno.ncol, "\n")
+
+    if (geno.nrow < geno.ncol){
+      message("[Caution] Number of samples exceeding number of measured features,
+              please check rows and columns of <genotype.mx> \n")
+      message("- If you are from the future and have more samples than measured features,
+              disregard the above message and please proceed. \n")
+    }
+
+    if(!is.null(info.df)){
+
+      message("Checking dimensions and column/row names \n")
+
+      if(nrow(info.df) == ncol(genotype.mx)){
+        message("[Good] Number of samples in <info.df> and <genotype.mx> match \n")
+      } else {
+        stop("Error: Sample numbers in <info.df> and <genotype.mx> don't match. Stopping script")
+      }
+
+      matching.names <- sum(rownames(info.df) %in% colnames(genotype.mx))
+
+      if(matching.names == ncol(genotype.mx)){
+        message("[Good] All samples in <genotype.mx> are accounted for in <info.df> \n")
+      } else {
+        stop("Missing sample Information: Check rownames of <info.df> and column names of <genotype.mx>")
+      }
+    }
+
+    if(!is.null(info.df) & is.null(check.covars)){
+      message("- Sample info detected but missing input for <check.covars> \n")
+      message("- Using column names of <info.df> as <check.covars> \n")
+      check.covars <- colnames(info.df)
+    }
+
+    message("Pre Processing Data \n")
+    # removing NA values, centering, scaling (optional)
+    genotype.mx <- pre_process_data(genotype.mx)
+
+    message("Running PCA \n")
+    pca.result <- prcomp(t(genotype.mx))
+
+    # calculating percent variance each component explains
+    pca.result$var.percent <- (pca.result$sdev^2 / sum(pca.result$sdev^2)) * 100
+
+
+    pca.result$peaks <- apply(pca.result$rotation, 2, peak_detection)
+
+    if(!is.null(check.covars) & !is.null(info.df)){
+      message("Checking association between covariates and components\n")
+      # Anova analysis for covariates vs ICA weights (A matrix)
+      pca.result$cov.pval.mx <- component_association_test(t(pca.result$x),info.df,check.covars)
+
+      # get bonferroni corrected threshold
+      cor.threshold <- cor.threshold / (length(pca.result$cov.pval.mx))
+
+      # row and column positions for significant p-values
+      corr.idx <- which(pca.result$cov.pval.mx < cor.threshold, arr.ind = T)
+    } else{
+      message("No <info.df> supplied, association test skipped.\n")
+      corr.idx <- NULL
+    }
+
+    if(length(corr.idx) != 0 ){
+
+      # get PCs that were correlated to a covariate
+      correlated.pc <- unique(corr.idx[,1])
+
+      # Initiate dataframe to save results
+      covariate.corr.df <- data.frame(matrix(nrow = length(correlated.pc),ncol = 3))
+      colnames(covariate.corr.df) <- c("PC","Covariate.idx","Covariate.Name")
+
+      for( c in 1:length(correlated.pc)){
+        # get the PC number
+        pc.index <- correlated.pc[c]
+
+        # get the most significantly correlated covariate
+        covar.index <- which.min(pca.result$cov.pval.mx[pc.index,])
+        covariate.corr.df[c,"PC"] <- pc.index
+        covariate.corr.df[c,"Covariate.idx"] <- covar.index
+        covariate.corr.df[c,"Covariate.Name"] <- names(covar.index)
+      }
+
+      pca.result$cov.corr.idx <- covariate.corr.df
+
+      # Add variance information
+      pca.result$cov.corr.idx$var <- pca.result$var.percent[pca.result$cov.corr.idx$PC]
+
+      # remove temprary objects just for clarity
+      rm(covariate.corr.df, covar.index, pc.index, c)
+    } else {
+      pca.result$cov.corr.idx <- NULL     # in case there are no associated covariates
+    }
+
+    # save sample information in output object
+    pca.result$info.df <- info.df
+
+    # label result as pca for report2me() function
+    attr(pca.result, 'method') <- "pca"
+    return(pca.result)
+}
--- R/pre_process_data.R
+++ R/pre_process_data.R
@@ -1,48 +1,48 @@
 #' Pre-processing of the data
 #'
-#' The function takes in a g x N matrix, where g is the number of genes and N is the number of samples,
+#' The function takes in a g x N matrix, where g is the number of features and N is the number of samples,
 #' and removes any 0 variance entries followed by mean centering.
 #' If scaling option is true it will scale all rows to have unit standard deviation.
 #'
-#' @param phenotype.mx Phenotype matrix with a dimension of g X N  \code{phenotype.mx}
-#' @param scale.pheno If TRUE phenotypes are scaled to have unit standard deviation.
-#' @return Pre-processed phenotype matrix
+#' @param input_matrix Input matrix with a dimension of g X N  \code{input_matrix}
+#' @param scale_features If TRUE features are scaled to have unit standard deviation.
+#' @return Pre-processed data matrix
 #' @keywords keywords
 #'
 #'
 #' @examples
 #' raw.data <- rbind(rnorm(100,2,3),rnorm(100,10,2))
-#' centered <- pre_process_data(raw.data, scale.pheno = FALSE)
+#' centered <- pre_process_data(raw.data, scale_features = FALSE)
 #' apply(centered, 1, mean)
 #' apply(centered, 1, sd)
-#' centered.scaled <- pre_process_data(raw.data, scale.pheno = TRUE)
+#' centered.scaled <- pre_process_data(raw.data, scale_features = TRUE)
 #' apply(centered.scaled, 1, mean)
 #' apply(centered.scaled, 1, sd)
 #' @export
-pre_process_data <- function(phenotype.mx, scale.pheno = TRUE){
-    var.vec <- apply(phenotype.mx, 1, var)
+pre_process_data <- function(input_matrix, scale_features = TRUE){
+    var.vec <- apply(input_matrix, 1, var)
     var0.count <- sum(var.vec == 0)
 
 
-    # Remove 0 variance genes if existing
+    # Remove 0 variance  if existing
     if(var0.count != 0){
-        message("- Removing ",var0.count," 0 variance genes \n")
-        phenotype.mx <- phenotype.mx[-c(which(var.vec == 0)),]
+        message("- Removing ",var0.count," 0 variance features \n")
+        input_matrix <- input_matrix[-c(which(var.vec == 0)),]
     } else {
-        message("- No 0 variance genes in dataset \n")
+        message("- No 0 variance features in dataset \n")
     }
 
 
-    if(scale.pheno == TRUE){
-        message("- Centering and Scaling Phenotype Matrix \n")
-        # scale phenotype matrix to have 0 mean and 1 sd
-        phenotype.mx <- t(apply(phenotype.mx, 1, function(x) (x - mean(x))/sd(x)))
+    if(scale_features == TRUE){
+        message("- Centering and Scaling Input Matrix \n")
+        # scale Input matrix to have 0 mean and 1 sd
+        input_matrix <- t(apply(input_matrix, 1, function(x) (x - mean(x))/sd(x)))
     } else {
-        message("- Centering Phenotype Matrix \n")
-        phenotype.mx <- t(apply(phenotype.mx, 1, function(x) (x - mean(x))))
+        message("- Centering Input Matrix \n")
+        input_matrix <- t(apply(input_matrix, 1, function(x) (x - mean(x))))
     }
 
-    message("- Dimension of Phenotype Matrix = [",dim(phenotype.mx)[1],",",dim(phenotype.mx)[2],"]\n")
-    return(phenotype.mx)
+    message("- Dimension of Input Matrix = [",dim(input_matrix)[1],",",dim(input_matrix)[2],"]\n")
+    return(input_matrix)
 }
 
