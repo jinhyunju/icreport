110659b0a0eda29396716c78458dad1d31ec73c5
Jin-Lab workstation <jinhyun.ju@gmail.com>
2016-05-06 13:57:14 -0400

    fixed type

--- R/h5_ica.R
+++ R/h5_ica.R
@@ -220,7 +220,7 @@ ica_genotype_association <- function(ica_list = NULL,
   #genetic.factors <- colnames(ic.vs.geno)[unique(sig[,"col"])]
   #non.genetic <- colnames(ic.vs.geno)[which(!(colnames(ic.vs.geno) %in% genetic.factors))]
   ica_list$geno_pvals <- ic.vs.geno
-  ica_list$geno_cor_count <- apply(ic.vs.genos, 2, function(x) sum(na.omit(x < (sig_threshold / length(ic.vs.geno)))))
+  ica_list$geno_cor_count <- apply(ic.vs.geno, 2, function(x) sum(na.omit(x < (sig_threshold / length(ic.vs.geno)))))
   ica_list$non_genetic <- names(ica_list$geno_cor_count[which(ica_list$geno_cor_count < 1)])
   attr(ica_list, 'geno_cor') <- "yes"
   return(ica_list)
